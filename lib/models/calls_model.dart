faad55005845a2167ff50b09c6fa330f2cedd586
Nikhil Sharma <nsharma3400@gmail.com>
2020-10-17 22:16:33 +0530

    calls models updated

--- lib/models/calls_model.dart
+++ lib/models/calls_model.dart
@@ -2,16 +2,30 @@ class CallModel{
   final String name;
   final String dateAndTime;
   final String avatarUrl;
-  final String call;
+  
 
-  CallModel({this.name,this.dateAndTime,this.call,this.avatarUrl});
+  CallModel({this.name,this.dateAndTime,this.avatarUrl});
 }
 
 List<CallModel> callData = [
   new CallModel(
     name: "Nikhil",
     dateAndTime: "October 12, 4:18 PM",
-    call: "Icons.video_call",
+    avatarUrl: "https://image.flaticon.com/icons/png/512/17/17004.png",
+  ),
+  new CallModel(
+    name: "Aakash",
+    dateAndTime: "October 10, 7:08 PM",
+    avatarUrl: "https://image.flaticon.com/icons/png/512/17/17004.png",
+  ),
+  new CallModel(
+    name: "Keshav",
+    dateAndTime: "October 1, 6:07 PM",
+    avatarUrl: "https://image.flaticon.com/icons/png/512/17/17004.png",
+  ),
+  new CallModel(
+    name: "Aryan",
+    dateAndTime: "October 12, 7:18 PM",
     avatarUrl: "https://image.flaticon.com/icons/png/512/17/17004.png",
   ),
 ];
